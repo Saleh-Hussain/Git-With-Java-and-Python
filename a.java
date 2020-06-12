816f62b11ef218a4d497b3e7ad41e96d58bf1c87
Saleh <s101605621@aou.edu.sa>
2020-06-12 18:28:56 +0300

    The comment change

--- a.java
+++ a.java
@@ -3,5 +3,6 @@ public class a{
 		
 		System.out.println("Hello from Git!!!");
 		System.out.println("Now this is the new update... v2");
+		// this is just a comment, but should it be considered as a change to the file?? We'll see...
 	} // end main()
 } // end class a
