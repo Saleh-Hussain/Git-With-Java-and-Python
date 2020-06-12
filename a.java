683cfc602e3f71f172db3d48b42c24c0e2057d1f
Saleh <s101605621@aou.edu.sa>
2020-06-12 18:22:42 +0300

    The changes are in the code

--- a.java
+++ a.java
@@ -2,5 +2,6 @@ public class a{
 	public static void main (String[] args){
 		
 		System.out.println("Hello from Git!!!");
+		System.out.println("Now this is the new update... v2");
 	} // end main()
 } // end class a
